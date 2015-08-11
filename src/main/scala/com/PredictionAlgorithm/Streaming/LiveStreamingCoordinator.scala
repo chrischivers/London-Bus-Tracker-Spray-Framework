f3d54e27d21ac04cb287f969c55ca154e0affb5b
chrischivers <chrischivers@gmail.com>
2015-08-11 19:13:36 +0100

    Refactored LiveStreamingCoordinator into Interface and implementation

--- src/main/scala/com/PredictionAlgorithm/Streaming/LiveStreamingCoordinator.scala
+++ src/main/scala/com/PredictionAlgorithm/Streaming/LiveStreamingCoordinator.scala
@@ -14,70 +14,31 @@ import scala.concurrent.duration._
 
 case class PackagedStreamObject(reg:String, nextArrivalTime: String, decodedPolyLineToNextStop: Array[(String,String)], route_ID: String, direction_ID: Int, towards:String, nextStopID: String, nextStopName: String)
 
-object LiveStreamingCoordinator {
+object LiveStreamingCoordinator extends LiveStreamingCoordinatorInterface {
 
-  implicit val actorSystem = ActorSystem("live_streaming")
-  @volatile private var streamList:mutable.ListBuffer[FIFOStreamImplementation] = ListBuffer()
-  implicit val timeout = 1000
-
-  private var liveActors = Map[String, (ActorRef, Long)]()
   private var inputsReceivedCache: List[(String, String, Int, String, Long)] = List()
-  private val CACHE_HOLD_FOR_TIME = 600000
-  private val IDLE_TIME_UNTIL_ACTOR_KILLED = 600000
-  @volatile private var cleaningInProgress: Boolean = false
 
-   def setObjectPosition(liveSourceLine: TFLSourceLine): Unit = {
+
+  override def setObjectPosition(liveSourceLine: TFLSourceLine): Unit = {
     if (liveSourceLine.route_ID == "3") {
-    // This checks it is not aready in the cache
-    if (!inputsReceivedCache.exists(x => x._1 == liveSourceLine.vehicle_Reg && x._2 == liveSourceLine.route_ID && x._3 == liveSourceLine.direction_ID && x._4 == liveSourceLine.stop_Code)) {
-      inputsReceivedCache = inputsReceivedCache :+(liveSourceLine.vehicle_Reg, liveSourceLine.route_ID, liveSourceLine.direction_ID, liveSourceLine.stop_Code, System.currentTimeMillis())
-      inputsReceivedCache = inputsReceivedCache.filter(x => x._5 > (System.currentTimeMillis() - CACHE_HOLD_FOR_TIME))
+      // This checks it is not aready in the cache
+      if (!inputsReceivedCache.exists(x => x._1 == liveSourceLine.vehicle_Reg && x._2 == liveSourceLine.route_ID && x._3 == liveSourceLine.direction_ID && x._4 == liveSourceLine.stop_Code)) {
+        inputsReceivedCache = inputsReceivedCache :+(liveSourceLine.vehicle_Reg, liveSourceLine.route_ID, liveSourceLine.direction_ID, liveSourceLine.stop_Code, System.currentTimeMillis())
+        inputsReceivedCache = inputsReceivedCache.filter(x => x._5 > (System.currentTimeMillis() - CACHE_HOLD_FOR_TIME))
 
-      val vehicleReg = liveSourceLine.vehicle_Reg
-      if (liveActors.contains(vehicleReg)) {
-        liveActors(vehicleReg)._1 ! liveSourceLine
-      } else {
+        val vehicleReg = liveSourceLine.vehicle_Reg
+        if (liveActors.contains(vehicleReg)) {
+          liveActors(vehicleReg)._1 ! liveSourceLine
+        } else {
 
-        val newActor: ActorRef = actorSystem.actorOf(Props(new VehicleActor(vehicleReg)), vehicleReg)
-        this.synchronized {
-          liveActors = liveActors + (vehicleReg ->(newActor, System.currentTimeMillis()))
+          val newActor: ActorRef = actorSystem.actorOf(Props(new VehicleActor(vehicleReg)), vehicleReg)
+          this.synchronized {
+            liveActors = liveActors + (vehicleReg ->(newActor, System.currentTimeMillis()))
+          }
+          newActor ! liveSourceLine //Start it off
         }
-        newActor ! liveSourceLine //Start it off
       }
     }
-     }
-  }
-
-  def registerNewStream(streamImpl: FIFOStreamImplementation): Unit = {
-    println("new stream registered")
-    streamList += streamImpl
-  }
-
-  def getNumberLiveActors = liveActors.size
-
-  def updateLiveActorTimestamp(reg: String) = {
-    this.synchronized {
-      liveActors = liveActors + (reg ->(liveActors(reg)._1, System.currentTimeMillis()))
-    }
-    //if (!cleaningInProgress) cleanUpLiveActorsList
-  }
-
-  def cleanUpLiveActorsList = {
-    this.synchronized {
-      cleaningInProgress = true
-      val actorsToKill = liveActors.filter(x => x._2._2 < (System.currentTimeMillis() - IDLE_TIME_UNTIL_ACTOR_KILLED))
-      actorsToKill.foreach(x => {
-        x._2._1 ! PoisonPill //Kill actor
-        enqueue(new PackagedStreamObject("","",Array(),"",0,"","","")) //Send kill to stream Queue
-        liveActors = liveActors - x._1
-        println("ActorKilled: " + x._1)
-      })
-      cleaningInProgress = false
-    }
-  }
-
-  def enqueue(pso: PackagedStreamObject) =  {
-    streamList.foreach(x=> x.enqueue(pso))
   }
 
 }
--- src/main/scala/com/PredictionAlgorithm/Streaming/LiveStreamingCoordinatorInterface.scala
+++ src/main/scala/com/PredictionAlgorithm/Streaming/LiveStreamingCoordinatorInterface.scala
@@ -1,19 +1,61 @@
 package com.PredictionAlgorithm.Streaming
 
+import akka.actor.{ActorSystem, ActorRef}
 import com.PredictionAlgorithm.DataSource.TFL.TFLSourceLine
+import com.PredictionAlgorithm.Spray.FIFOStreamImplementation
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 case class livePositionData(routeID: String, directionID: Int, pointSequence: Int, nextStopID: String, arrivalTime: Long, firstLast:Option[String])
 
 trait LiveStreamingCoordinatorInterface {
 
-  //Reg_No -> routeID, direction ID, pointSequence, nextStopCode, timeUntilNextStop
-  var livePositionMap:Map[String,livePositionData]
-
-  // Map of RegNumber (Unique vehicle identifier) to Stream Result
-  def getObjectPositionsMap: Map[String, LiveStreamResult]
+  var liveActors = Map[String, (ActorRef, Long)]()
+  implicit val actorSystem = ActorSystem("live_streaming")
+  @volatile var streamList:mutable.ListBuffer[FIFOStreamImplementation] = ListBuffer()
+  implicit val timeout = 1000
+  val CACHE_HOLD_FOR_TIME = 600000
+  val IDLE_TIME_UNTIL_ACTOR_KILLED = 600000
+  @volatile private var cleaningInProgress: Boolean = false
 
   def setObjectPosition(liveSourceLine: TFLSourceLine)
 
-  def getPositionMapSize: Int = livePositionMap.size
+  def registerNewStream(streamImpl: FIFOStreamImplementation): Unit = {
+    println("new stream registered (" + streamImpl + ")")
+    streamList += streamImpl
+  }
+
+  def deregisterStream(streamImpl: FIFOStreamImplementation) = {
+    println("stream deregistered (" + streamImpl + ")")
+    streamList -= streamImpl
+  }
+
+
+  def getNumberLiveActors = liveActors.size
+
+  def updateLiveActorTimestamp(reg: String) = {
+    this.synchronized {
+      liveActors = liveActors + (reg ->(liveActors(reg)._1, System.currentTimeMillis()))
+    }
+    //if (!cleaningInProgress) cleanUpLiveActorsList
+  }
+  def enqueue(pso: PackagedStreamObject) =  {
+    streamList.foreach(x=> x.enqueue(pso))
+  }
+
+  def cleanUpLiveActorsList = {
+    this.synchronized {
+      cleaningInProgress = true
+      val actorsToKill = liveActors.filter(x => x._2._2 < (System.currentTimeMillis() - IDLE_TIME_UNTIL_ACTOR_KILLED))
+      actorsToKill.foreach(x => {
+        x._2._1 ! PoisonPill //Kill actor
+        enqueue(new PackagedStreamObject("","",Array(),"",0,"","","")) //Send kill to stream Queue
+        liveActors = liveActors - x._1
+        println("ActorKilled: " + x._1)
+      })
+      cleaningInProgress = false
+    }
+  }
 
 }
